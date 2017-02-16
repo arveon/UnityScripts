84ae9dc82571ca3426c90f2113dc7c4705d70498
arveon <aloginovs@dundee.ac.uk>
2017-02-16 14:18:04 +0000

    Scripts for camera rotation following the mouse and moving character with keyboard

---
+++ CamRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotation : MonoBehaviour {
+
+	private Vector2 mouseLook;
+
+	public float sensitivity = 10f;
+
+	GameObject character;
+
+	// Use this for initialization
+	void Start ()
+	{
+		character = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector2 mouse_delta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+		mouse_delta *= sensitivity;
+
+		mouseLook += mouse_delta;
+
+		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+	}
+}
---
+++ CharMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+	public float movement_speed = 5f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float x_mov = Input.GetAxis("Horizontal") * movement_speed * Time.deltaTime;
+		float z_mov = Input.GetAxis("Vertical") * movement_speed * Time.deltaTime;
+		float y_mov = Input.GetAxis("Jump") * movement_speed * Time.deltaTime;
+
+
+		transform.Translate(x_mov, y_mov, z_mov);
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+			Cursor.lockState = CursorLockMode.None;
+	}
+}
